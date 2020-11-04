9e441ba3a814951e390f45a55774b4d15f46e9dd
JuanjoMorell <juanjo.morell7@gmail.com>
2020-11-04 10:34:20 +0100

    Practica 1 v2

--- practica1.hs
+++ practica1.hs
@@ -26,6 +26,8 @@ maximoc' x y
 -----------------------------------------EJERCICIO 2 
 ----------------------------------------------------
 
+areaCirculo :: Float -> Float
+areaCirculo n = (22/7) * n^2
 
 ----------------------------------------------------
 -----------------------------------------EJERCICIO 3 
@@ -40,13 +42,56 @@ fib2 n = fib2 (n-1) + fib2 (n-2)
 -----------------------------------------EJERCICIO 4 
 ----------------------------------------------------
 
+absol :: Integer -> Integer
+absol n = if n<0 then -n else n
 
 ----------------------------------------------------
 -----------------------------------------EJERCICIO 5 
 ----------------------------------------------------
 
+aumentar, aumentar2 :: Integer -> Integer
+aumentar n = (n+1)^2
+aumentar2 n = (n^2)+1
+
+sucesor :: Integer -> Integer
+sucesor = (+1)
+
+cuadrado :: Integer -> Integer
+cuadrado n = n * n
+
+aumentar':: Integer -> Integer
+aumentar' = cuadrado.sucesor
+
+aumentar2' :: Integer -> Integer
+aumentar2' = sucesor.cuadrado
 
 ----------------------------------------------------
 -----------------------------------------EJERCICIO 6 
+----------------------------------------------------
+
+nAnd :: Bool -> Bool -> Bool
+nAnd True True = False
+nAnd x y = True
+
+nAnd' :: Bool -> Bool -> Bool
+nAnd' x y
+    | x && y = False
+    | otherwise = True
+
+----------------------------------------------------
+-----------------------------------------EJERCICIO 7 
+----------------------------------------------------
+
+xor :: Bool -> Bool -> Bool
+xor False True = True
+xor True False = True
+xor x y = False
+
+xor' :: Bool -> Bool -> Bool
+xor' False x = x
+xor' True x = not x
+
+----------------------------------------------------
+-----------------------------------------EJERCICIO 8 
 ----------------------------------------------------
 
